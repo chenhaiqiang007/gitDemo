cf07d9f196c2de6dc2f36223af6eb6ec364cc3b9
“乔岚生” <qiaolansheng@icloud.com>
2018-06-26 10:44:11 +0800

    Initial Commit

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by guoguoxian on 2018/6/26.
+//  Copyright © 2018年 chenhaiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
